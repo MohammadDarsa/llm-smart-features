1ad88902fe7c94f9dfdcf35310737f010fc06258
darsa <darsafxz@gmail.com>
2023-09-24 00:10:53 +0300

    refactoring

---
+++ language_model/memory/vector_db_config.py
@@ -0,0 +1,31 @@
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.vectorstores import Chroma
+
+
+class VectorDBConfig:
+    def __init__(self, db_path='./documents', chunk_size=1000, chunk_overlap=20, batch_size=1):
+        self.db_path = db_path
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.batch_size = batch_size
+        self.db = self.config()
+        self.db.persist()
+
+    def config(self):
+        docs = self.load_docs()
+        split_docs = self.split_docs(docs)
+        embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={"device": "cpu"},
+                                                   encode_kwargs={"batch_size": self.batch_size})
+        return Chroma.from_documents(split_docs, embeddings, persist_directory="./db")
+
+    def load_docs(self):
+        loader = DirectoryLoader(self.db_path, loader_cls=TextLoader)
+        loaded_docs = loader.load()
+        return loaded_docs
+
+    def split_docs(self, doc):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
+        split_doc = text_splitter.split_documents(doc)
+        return split_doc
--- language_model/routers/chat_api.py
+++ language_model/routers/chat_api.py
@@ -1,12 +1,31 @@
 from fastapi import APIRouter
+from langchain import PromptTemplate, LLMChain
+
 from language_model.model.request.chat_request import ChatRequest
 
 
 class ChatController:
-    def __init__(self, conversation):
-        self.conversation = conversation
+    def __init__(self, llm_config):
+        self.llm_config = llm_config
         self.router = APIRouter()
         self.router.add_api_route("/chat", self.chat, methods=["POST"])
 
     async def chat(self, request: ChatRequest):
-        return {"message": self.conversation({"question": request.text})}
+        _DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+        Relevant pieces of previous conversation:
+        {history}
+
+        (You do not need to use these pieces of information if not relevant)
+
+        Current conversation:
+        Human: {question}"""
+        prompt = PromptTemplate.from_template(template=_DEFAULT_TEMPLATE)
+        conversation = LLMChain(
+            llm=self.llm_config.local_llm,
+            prompt=prompt,
+            verbose=True,
+            memory=self.llm_config.memory
+        )
+
+        return {"message": conversation({"question": request.text})}
