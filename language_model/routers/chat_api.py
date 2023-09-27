579052fc485b85fbaa9afc15da33be740e9e898e
darsa <darsafxz@gmail.com>
2023-09-27 14:27:28 +0300

    updated prompt

--- language_model/routers/chat_api.py
+++ language_model/routers/chat_api.py
@@ -34,24 +34,26 @@ class ChatController:
         products_str = json.dumps(products)
 
         # create a template
-        _DEFAULT_TEMPLATE = """
-            You're a shop keeper in an electronics store. A customer comes in and asks for a phone with the following specs, use the headers and products listed below to find the most suitable phone. The attributes in the headers and products are separated by 2 spaces:
-            The user will input a query and you should find the most suitable phone or phones.
-            The Output should look like this only conatining ids and nothing else: 
-            id: 1, 3, 5;
-            
-            Multiple products can be returned.
-            
-            Here's the header list:
-            
-            {headers}
+        _DEFAULT_TEMPLATE = """You're a shop keeper in an electronics store. A customer comes in and asks for a phone with the following specs presented in the query.
+Here's the product list containing all the attributes as key value pair separated by a column (:). The user will search on these key value pairs, The products are separated by a semi-column(;):
+{products_str}
 
-            Here's the product list, each attribute is separated by 2 spaces and is associated to its place in the headers:
+The user will input a query and you should find the most suitable phone or phones.
 
-            {products_str}
+The answer to the customer's query should only include the ids of the phones that matches the query and nothing else. The answer should only contain ths ids.
 
-            Query: {query}
-        """
+Examples of query-question:
+
+query: "google branded phones"
+answer: "3"
+
+query: "Phones with more than 20MP camera:
+answer: "1,2,3"
+
+Answer this query of the user:
+
+query: {query}
+answer:"""
         prompt = PromptTemplate(template=_DEFAULT_TEMPLATE, input_variables=["products_str", "query"])
         conversation = LLMChain(
             llm=self.llm_config.local_llm,
