1dd27676d00aee319b6f9ddccd9aacdfc1c23089
darsa <darsafxz@gmail.com>
2023-10-01 20:05:10 +0300

    updated 70b model

--- language_model/routers/chat_api.py
+++ language_model/routers/chat_api.py
@@ -2,6 +2,7 @@ import json
 
 from fastapi import APIRouter
 from langchain import PromptTemplate, LLMChain
+from langchain.memory import VectorStoreRetrieverMemory
 
 from language_model.model.request.chat_request import ChatRequest
 
@@ -14,7 +15,8 @@ class ChatController:
     def __init__(self, llm_config):
         self.llm_config = llm_config
         self.router = APIRouter()
-        self.router.add_api_route("/chat", self.smart_search, methods=["POST"])
+        self.router.add_api_route("/smart-search", self.smart_search, methods=["POST"])
+        self.router.add_api_route("/chat", self.chat, methods=["POST"])
 
     async def smart_search(self, request: ChatRequest):
         # get the products
@@ -52,3 +54,28 @@ answer:"""
         )
 
         return {"message": conversation({"query": request.text, "products_str": products_str})}
+
+    async def chat(self, request: ChatRequest):
+        # Memory config
+        self.llm_config.memory.save_context({"input": "My favorite food is pizza"}, {"output": "that's good to know"})
+
+        # Template config
+        _DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+        Relevant pieces of previous conversation:
+        {history}
+
+        (You do not need to use these pieces of information if not relevant)
+
+        Current conversation:
+        Human: {question}"""
+
+        prompt = PromptTemplate.from_template(template=_DEFAULT_TEMPLATE)
+        conversation = LLMChain(
+            llm=self.llm_config.local_llm,
+            prompt=prompt,
+            verbose=True,
+            memory=self.llm_config.memory
+        )
+
+        return {"message": conversation({"question": request.text})}
